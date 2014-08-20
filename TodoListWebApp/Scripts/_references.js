e364e5a88d8064b5e1d3362702bb2fa56152799b
dstrockis <dastrock@microsoft.com>
2014-08-20 08:47:04 -0700

    Katana GA Update

--- TodoListWebApp/Scripts/_references.js
+++ TodoListWebApp/Scripts/_references.js
@@ -1,6 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="bootstrap.js" />
 /// <reference path="jquery-2.1.1.js" />
-/// <reference path="modernizr-2.7.2.js" />
 /// <reference path="respond.js" />
 /// <reference path="respond.matchmedia.addlistener.js" />
+/// <reference path="modernizr-2.8.3.js" />
