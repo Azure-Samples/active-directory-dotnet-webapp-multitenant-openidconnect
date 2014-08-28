1a34bb038998b4a339f56e559483545e09a6944f
dstrockis <dastrock@microsoft.com>
2014-08-28 10:49:33 -0700

    v2 GA MyGet Update

---
+++ packages/Microsoft.IdentityModel.Clients.ActiveDirectory.2.9.10828.0745/src/src/ADAL.Common/CommonAssemblyInfo.cs
@@ -0,0 +1,34 @@
+//----------------------------------------------------------------------
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved
+// Apache License 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------
+using System.Reflection;
+[assembly: AssemblyProduct("Active Directory Authentication Library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("2.9.0.0")]
+// Keep major and minor versions in AssemblyFileVersion in sync with AssemblyVersion.
+// Build and revision numbers are replaced on build machine for official builds.
+[assembly: AssemblyFileVersion("2.9.10828.0745")]
+// On official build, attribute AssemblyInformationalVersionAttribute is added as well
+// with its value equal to the hash of the last commit to the git branch.
+// e.g.: [assembly: AssemblyInformationalVersionAttribute("4392c9835a38c27516fc0cd7bad7bccdcaeab161")]
+ 
+[assembly: AssemblyInformationalVersionAttribute("d79c059ecbe81eb8994281904e67d572a6868940")] 
